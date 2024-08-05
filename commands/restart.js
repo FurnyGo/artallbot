7b75d8b47421719d872d90b2fd1afb1cb5af00b8
o/ <68079109+FurnyGo@users.noreply.github.com>
2024-08-05 10:10:43 +0400

    new

--- commands/restart.js
+++
@@ -1,6 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('restart')
-        .setDescription('Перезапуск бота')
-};
