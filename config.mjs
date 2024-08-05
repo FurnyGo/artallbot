cdab3eec4a5517aa72c8363d1f5d2da8a2e041af
o/ <68079109+FurnyGo@users.noreply.github.com>
2024-08-05 10:15:06 +0400

    new

--- config.mjs
+++ config.mjs
@@ -1,8 +1,8 @@
-export var clientId = "";
-export var notsafe = "";
-export var minenick = "";
-export var minepass = "";
-export var admins = "";
-export var seeker_key = "";
-export var log_channel = "";
-export var parts_channel = "";
+export var clientId = "Дискорд айди бота";
+export var notsafe = "Дискорд токен бота";
+export var minenick = "Ник аккаунта с донатом для генерации картинок (на маседе)";
+export var minepass = "Пароль от аккаунта";
+export var admins = "Айди админов бота (права на /restart и бесконечный лимит на размер картинок) (разделять через пробел)";
+export var seeker_key = "Токен от ServerSeeker (команды /server, /find)";
+export var log_channel = "Айди канала в дс куда будет писаться о том кто какую команду юзает и какую картинку загружает";
+export var parts_channel = "Айди канала в дс куда будут загружаться части картинки для загрузки на сервер";
